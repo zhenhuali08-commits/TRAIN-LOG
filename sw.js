f93af1aad790fe9aa4f279aa4cd4019b9d04dd9e
zhenhuali08-commits <zhenhuali08@gmail.com>
2026-08-16 16:34:42 +0800

    Upgrade recovery storage and training insights

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const APP_CACHE='train-log-v30-local';
-const MEDIA_CACHE='train-log-media-v30';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./assets/icon-192.png','./assets/icon-512.png','./assets/heatmap-neutral-v2.png'];
+const APP_CACHE='train-log-v31-local';
+const MEDIA_CACHE='train-log-media-v31';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./assets/icon-192.png','./assets/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(APP_CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>![APP_CACHE,MEDIA_CACHE].includes(k)).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
